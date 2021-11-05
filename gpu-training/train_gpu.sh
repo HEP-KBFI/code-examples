50b6e4d05cc7da132175acca9bab7a9edcdd52dc
Joosep Pata <joosep.pata@cern.ch>
2021-11-05 14:42:54 +0200

    up

--- gpu-training/train_gpu.sh
+++ gpu-training/train_gpu.sh
@@ -3,7 +3,11 @@
 #SBATCH --gpus 1
 #SBATCH --mem-per-gpu=8G
 #SBATCH --no-requeue
+
+#see https://github.com/HEP-KBFI/singularity/blob/master/README.md
+#to read about how to install additional packages
 IMG=/home/software/singularity/tf26.simg:latest
+
 cd ~/code-examples/gpu-training
 singularity exec -B /scratch-persistent --nv $IMG python3 train.py
 
