5f703e87c57bab7e397f53f7ac6c170873f07d9a
Joosep Pata <joosep.pata@cern.ch>
2022-12-23 13:58:39 +0200

    use either cupy or numpy

--- gpu/cuda-mp.py
+++ gpu/cuda-mp.py
@@ -1,7 +1,13 @@
-import cupy
+USE_CUDA=True
 
-#import numpy as cupy
-#cupy.asnumpy = lambda x: x
+if USE_CUDA:
+    import cupy as npcp
+    DeviceContext = npcp.cuda.Device
+else:
+    import numpy as npcp
+    npcp.asnumpy = lambda x: x
+    import contextlib
+    DeviceContext = contextlib.nullcontext
 
 import multiprocessing
 from multiprocessing import Process, Queue
@@ -17,14 +23,14 @@ def f(x):
     #get the device ID from the global context
     global device_id
     
-    print("f: process PID {}, GPU {}".format(multiprocessing.current_process().pid, device_id))
+    # print("f: process PID {}, GPU {}".format(multiprocessing.current_process().pid, device_id))
 
     #ensure computation is on the device allocated to this process
-    with cupy.cuda.Device(device_id):
-        y = x*cupy.ones((glob_var.shape[0], glob_var.shape[1], glob_var.shape[1]), dtype=cupy.float32)
+    with DeviceContext(device_id):
+        y = x*npcp.ones((glob_var.shape[0], glob_var.shape[1], glob_var.shape[1]), dtype=npcp.float32)
         ret = y @ glob_var
         ret = ret.swapaxes(1,2) @ y
-        ret = cupy.asnumpy(ret.mean())
+        ret = npcp.asnumpy(ret.mean())
     return ret
 
 def init_vars(queue):
@@ -33,18 +39,23 @@ def init_vars(queue):
 
     #get the device assigned to this process
     device_id = queue.get()
-    print("init_vars: process PID {}, GPU {}".format(multiprocessing.current_process().pid, device_id))
+    # print("init_vars: process PID {}, GPU {}".format(multiprocessing.current_process().pid, device_id))
 
     #allocate data on the device
-    with cupy.cuda.Device(device_id):
-        glob_var = cupy.ones((N, 6, 1), dtype=cupy.float32)
+    with DeviceContext(device_id):
+        glob_var = npcp.ones((N, 6, 1), dtype=npcp.float32)
 
 if __name__ == '__main__':
     multiprocessing.set_start_method('forkserver')
-   
-    cvd = os.environ["CUDA_VISIBLE_DEVICES"]
-    cvd = [int(x) for x in cvd.split(",")]
-    NUM_GPUS = len(cvd) 
+  
+    if USE_CUDA: 
+        cvd = os.environ["CUDA_VISIBLE_DEVICES"]
+        cvd = [int(x) for x in cvd.split(",")]
+        NUM_GPUS = len(cvd)
+    #actually no GPUs will be used, we just create 1xPROC_PER_GPU CPU processes
+    else:
+        NUM_GPUS = 1
+ 
     PROC_PER_GPU = 8
     queue = Queue()
     #even though CUDA_VISIBLE_DEVICES could be e.g. 3,4
@@ -57,18 +68,17 @@ if __name__ == '__main__':
     input_data = range(1000)
 
     pool = multiprocessing.Pool(NUM_GPUS*PROC_PER_GPU, initializer=init_vars, initargs=(queue, ))
-    ret = sum(pool.map(f, input_data))
-    print(ret)
 
-    ret = sum(pool.map(f, input_data))
-    print(ret)
+    #calculate the output 3 times
+    for i in range(3):
+        ret = sum(pool.map(f, input_data))
+        print(ret)
 
-    ret = sum(pool.map(f, input_data))
-    print(ret)
-
+    #ensure processes are closed
     pool.close()
     pool.join()
 
+    #get the time
     t1 = time.time()
     print(t1 - t0)
 
