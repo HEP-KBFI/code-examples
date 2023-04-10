51953e8eb739776ab7dbc0906ddb5cae7607998c
Joosep Pata <joosep.pata@cern.ch>
2023-04-10 15:16:41 +0300

    Update train_gpu_tf.sh

--- gpu-training/train_gpu_tf.sh
+++ gpu-training/train_gpu_tf.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH -p gpu
-#SBATCH --gpus 1
+#SBATCH --gres gpu:rtx:1
 #SBATCH --mem-per-gpu=8G
 #SBATCH --no-requeue
 
 #see https://github.com/HEP-KBFI/singularity/blob/master/README.md
 #to read about how to install additional packages
-IMG=/home/software/singularity/tf-2.10.0.simg
+IMG=/home/software/singularity/tf-2.11.0.simg
 
 cd ~/code-examples/gpu-training
-singularity exec -B /scratch-persistent --nv $IMG python3 train_tf.py
+singularity exec -B /scratch/persistent --nv $IMG python3 train_tf.py
