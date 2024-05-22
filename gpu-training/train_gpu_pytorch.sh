fc14d60ea10dd8f36344dcd8e56c736b0ed82524
Joosep Pata <joosep.pata@cern.ch>
2024-05-22 14:48:47 +0300

    try mig

--- gpu-training/train_gpu_pytorch.sh
+++ gpu-training/train_gpu_pytorch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH -p gpu
-#SBATCH --gres gpu:rtx:1
+#SBATCH --gres gpu:mig:1
 #SBATCH --mem-per-gpu=8G
 #SBATCH --no-requeue
 
 #see https://github.com/HEP-KBFI/singularity/blob/master/README.md
 #to read about how to install additional packages
-IMG=/home/software/singularity/pytorch.simg
+IMG=/home/software/singularity/pytorch.simg\:2024-04-30
 
 cd ~/code-examples/gpu-training
 singularity exec -B /scratch/persistent --nv $IMG python3 train_pytorch.py
